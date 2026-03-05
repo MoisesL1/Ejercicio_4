949f5617ef7d2ff701b87a1eb0963a8b41efd950
Moises <moiseslopez1155@gmail.com>
2026-03-04 19:40:04 -0600

    Mostrar ciudad.

--- Program.cs
+++ Program.cs
@@ -12,5 +12,15 @@ for(int x = 0; x < 5; x++)
     ciudades.Add(codigo, ciudad);
 }
 
+Console.WriteLine("Ingrese el còdigo: ");
+codigo = int.Parse(Console.ReadLine());
 
+if (ciudades.ContainsKey(codigo) == true)
+{
+    Console.WriteLine(ciudades[codigo]);
+}
+
+else
+{
+    Console.WriteLine("Ingreso un codigo invalido. ");
 }
