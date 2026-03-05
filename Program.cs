ef3ae624c326d82c3fe3f36a381bc419178bb0dd
Moises <moiseslopez1155@gmail.com>
2026-03-04 19:39:32 -0600

    Ingreso datos.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Dictionary<int, string> ciudades = new Dictionary<int, string>();
+
+int codigo;
+
+for(int x = 0; x < 5; x++)
+{
+    Console.WriteLine("Ingrese el código de la ciudad: ");
+    codigo = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el nombre de la ciudad: ");
+    string ciudad = Console.ReadLine();
+
+    ciudades.Add(codigo, ciudad);
+}
+
+
+}
